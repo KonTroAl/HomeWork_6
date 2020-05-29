43e2b286fa4163db564ed9aaf61f8c7490300ce1
KonTroAl <kostya98295@mail.ru>
2020-05-29 22:12:12 +0500

    Complit Task_2

---
+++ 2.py
@@ -0,0 +1,11 @@
+class Road:
+    def __init__(self, length, width):
+        self._length = length
+        self._width = width
+
+    def my_func(selfs):
+        print(f"Масса асфальта для заданных параметров равна: {selfs._length * selfs._width * 0.025 * 5} т.")
+
+
+track = Road(5000, 20)
+track.my_func()
