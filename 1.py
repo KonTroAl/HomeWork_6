e8566decbbabc94c4ef86183db4c3ffcf634acaa
KonTroAl <kostya98295@mail.ru>
2020-05-31 22:16:01 +0500

    Format Task_1

--- 1.py
+++ 1.py
@@ -2,6 +2,12 @@ from time import sleep
 from itertools import cycle
 
 
+class bcolors:
+    red = '\033[91m'
+    yellow = '\033[93m'
+    green = '\033[92m'
+
+
 class TrafficLight:
     def __init__(self, color):
         self.__color = color
@@ -10,19 +16,19 @@ class TrafficLight:
         z = 0
         while True:
             if z < 3:
-                print(self.__color[0])
+                print(bcolors.red + self.__color[0])
                 sleep(7)
-                print(self.__color[1])
+                print(bcolors.yellow + self.__color[1])
                 sleep(2)
-                print(self.__color[2])
+                print(bcolors.green + self.__color[2])
                 sleep(7)
                 new_list = [i for i in self.__color[::-1]]
                 for el in range(1, 2):
-                    print(new_list[el])
+                    print(bcolors.yellow + new_list[el])
                     sleep(2)
                 z += 1
             else:
-                print(self.__color[0])
+                print(bcolors.red + self.__color[0])
                 break
 
 
