10af43d582bcbbcbaf180bd6dcad5de40f62dd44
KonTroAl <kostya98295@mail.ru>
2020-05-31 12:41:14 +0500

    Second version of Task_1 (Complit)

--- 1.py
+++ 1.py
@@ -8,20 +8,22 @@ class TrafficLight:
 
     def running(self):
         z = 0
-        for el in cycle("1"):
-            if z > 0:
+        while True:
+            if z < 3:
+                print(self.__color[0])
+                sleep(7)
+                print(self.__color[1])
+                sleep(2)
+                print(self.__color[2])
+                sleep(7)
+                new_list = [i for i in self.__color[::-1]]
+                for el in range(1, 2):
+                    print(new_list[el])
+                    sleep(2)
+                z += 1
+            else:
+                print(self.__color[0])
                 break
-            print(self.__color[0])
-            sleep(7)
-            print(self.__color[1])
-            sleep(2)
-            print(self.__color[2])
-            sleep(7)
-            print(self.__color[1])
-            sleep(2)
-            print(self.__color[0])
-            z += 1
-            continue
 
 
 start = TrafficLight(["красный", "желтый", "зеленый"])
